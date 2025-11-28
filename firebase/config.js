12da7e47c60373b337189cb0f838d9d3d7d19235
Jonas Duzo <jonas10duzo@gmail.com>
2025-11-28 13:30:12 -0300

    first commit

--- firebase/config.js
+++ firebase/config.js
@@ -3,12 +3,20 @@ const { getFirestore } = require('firebase/firestore');
 
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-app.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-app.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+    // apiKey: "YOUR_API_KEY",
+    // authDomain: "your-app.firebaseapp.com",
+    // projectId: "your-project-id",
+    // storageBucket: "your-app.appspot.com",
+    // messagingSenderId: "your-sender-id",
+    // appId: "your-app-id"
+    apiKey: "AIzaSyArSALy7X3BSeY15O2ByE4ydfAv7kqItUw",
+    authDomain: "campeonato-xadrez.firebaseapp.com",
+    databaseURL: "https://campeonato-xadrez-default-rtdb.firebaseio.com",
+    projectId: "campeonato-xadrez",
+    storageBucket: "campeonato-xadrez.firebasestorage.app",
+    messagingSenderId: "859281317018",
+    appId: "1:859281317018:web:d6170dd8aa7b187fa59323",
+    measurementId: "G-GHQ7YY7J8W"
 };
 
 // Inicializar Firebase
